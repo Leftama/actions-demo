8780c7b0a34fb129a09c1cefc334875aea4795ef
cras-1296 <caraya.salazar@gmail.com>
2025-07-15 00:13:12 -0400

    Test evalúa más casos

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,19 @@
 const sum = require('./sum');
 
-test('Suma dos numeros', () => {
+describe('Suma dos números', () => {
+  test('suma 1 + 2 para igualar 3', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  test('suma -1 + 1 para igualar 0', () => {
+    expect(sum(-1, 1)).toBe(0);
+  });
+
+  test('suma 0 + 0 para igualar 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('suma 10 + 15 para igualar 25', () => {
+    expect(sum(10, 15)).toBe(25);
+  });
 });
